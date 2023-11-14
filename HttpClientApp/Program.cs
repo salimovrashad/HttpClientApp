229cdac87e462b0f0953237f6e35c764afae06be
salimovrashad <selimovresad33@gmail.com>
2023-11-14 19:01:14 +0400

    add app

---
+++ HttpClientApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HttpClientApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+
+            Console.WriteLine("Input the link: ");
+            var result = await client.GetStringAsync(Console.ReadLine());
+            Console.WriteLine(result);
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
